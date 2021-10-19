acc22e1421e99659a496d60290c42993a46d29f0
Ahmad Dody <ahmad.ch@students.amikom.ac.id>
2021-10-19 16:47:43 +0700

    more Jetpack

--- app/src/main/java/com/acxdev/learnjetpack/MainActivity.kt
+++ app/src/main/java/com/acxdev/learnjetpack/MainActivity.kt
@@ -1,74 +1,563 @@
 package com.acxdev.learnjetpack
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.coroutines.launch
+import kotlin.math.PI
+import kotlin.math.atan
+import kotlin.math.atan2
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.cat)
-            val description = "Cat"
-            val title = "This is my cat dude"
-            Column {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(0.5F)
-                        .padding(16.dp)
-                ) {
+//            V1()
+//            V2()
+//            V3()
+//            V4()
+//            V5()
+//            V6()
+//            V7()
+//            V8()
+            V9()
+        }
+    }
+}
 
-                    ImageCard(painter = painter, contentDescription = description, title = title)
-                }
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                Color.Red,
-                                fontSize = 20.sp
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("ETPACK")
-                    },
-                    color = Color.Red,
-                    fontSize = 30.sp,
-                    modifier = Modifier.clickable {
-                        Toast.makeText(this@MainActivity, "allo cat", Toast.LENGTH_SHORT).show()
-                    }
+@ExperimentalComposeUiApi
+@Composable
+fun V9() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0Xff101010)),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                .padding(30.dp)
+        ) {
+            var volume by remember {
+                mutableStateOf(0f)
+            }
+            val barCount = 20
+            MusicKnob(modifier = Modifier.size(100.dp)) {
+                volume = it
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            VolumeBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp),
+                activeBars = (barCount * volume).roundToInt(),
+                barCount = barCount
+            )
+
+        }
+    }
+}
+
+@Composable
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars: Int = 0,
+    barCount: Int = 19
+) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * barCount)
+        }
+
+        Canvas(modifier = modifier) {
+            for (i in 0 until barCount) {
+                drawRoundRect(
+                    color = if (i in 0..activeBars) Color.Green else Color.DarkGray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius(0f)
                 )
-
             }
         }
+    }
+}s
+
+@ExperimentalComposeUiApi
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChange: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
+    }
+    var touchX by remember {
+        mutableStateOf(0f)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+
+    Image(painter = painterResource(id = R.drawable.music),
+        contentDescription = "Music Knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+                            val fixedAngle = if (angle in -180f..-limitingAngle) {
+                                360f + angle
+                            } else {
+                                angle
+                            }
+                            rotation = fixedAngle
+                            val percent = (fixedAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChange(percent)
+                            true
+                        } else false
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
+
+
+}
+
+@Composable
+fun V8() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressBar(percentage = 0.8f, number = 100)
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animationDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    var currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0F,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    Box(
+        modifier = Modifier.size(radius * 2f),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+
+}
+
+@Composable
+fun V7() {
+    var sizeState by remember {
+        mutableStateOf(200.dp)
+    }
+    val size by animateDpAsState(
+        targetValue = sizeState,
+        animationSpec =
+        tween(
+            durationMillis = 3000,
+            delayMillis = 300,
+            easing = FastOutLinearInEasing
+        )
+//        spring(
+//            Spring.DampingRatioHighBouncy
+//        )
+//        keyframes {
+//            durationMillis = 5000
+//            sizeState at 0 with LinearEasing
+//            sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//            sizeState * 2f at 5000
+//        }
+    )
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(
+                durationMillis = 2000
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(
+        modifier = Modifier
+            .size(size)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = {
+            sizeState += 50.dp
+        }) {
+            Text(text = "Increase Size")
+        }
+    }
+}
+
+var i = 0
+
+@Composable
+fun V6(backPressedDispatcher: OnBackPressedDispatcher? = null) {
+//    SideEffect {
+//        i++
+//    }
+//
+//    val callback = remember {
+//        object : OnBackPressedCallback(true){
+//            override fun handleOnBackPressed() {
+//
+//            }
+//        }
+//    }
+//    DisposableEffect(key1 = backPressedDispatcher) {
+//        backPressedDispatcher.addCallback(callback)
+//        onDispose {
+//            callback.remove()
+//        }
+//    }
+
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(scaffoldState = scaffoldState) {
+//        var counter by remember {
+//            mutableStateOf(0)
+//        }
+        var counter = produceState(initialValue = 0) {
+            kotlinx.coroutines.delay(3000L)
+            value = 4
+        }
+        val coroutineScope = rememberCoroutineScope()
+        if (counter.value % 5 == 0 && counter.value > 0) {
+            LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                scaffoldState.snackbarHostState.showSnackbar("Hello")
+            }
+//            coroutineScope.launch {
+//                scaffoldState.snackbarHostState.showSnackbar("Hello")
+//            }
+        }
+        Button(onClick = {
+        }) {
+            Text(text = "Click Me ${counter.value}")
+        }
+    }
+}
+
+@Composable
+fun V5() {
+//    val constraintSet = ConstraintSet {
+//        val greenBox = createRefFor("greenbox")
+//        val redBox = creatRefFor("redbox")
+//        con
+//    }
+}
+
+@Composable
+fun V4() {
+//    val scrollState = rememberScrollState()
+//    Column(
+//        modifier = Modifier.verticalScroll(scrollState)
+//    ) {
+//        for (i in 1..50) {
+//            Text(
+//                text = "Item $i",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(vertical = 24.dp)
+//            )
+//        }
+//    }
+    LazyColumn {
+        itemsIndexed(
+            listOf("this", "is", "jetpack", "Compose")
+        ) { index, item ->
+            Text(
+                text = item,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+        items(5000) {
+            Text(
+                text = "Item $it",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+    }
+
+
+}
+
+@Composable
+fun V3() {
+    val scaffoldState = rememberScaffoldState()
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+        ) {
+            TextField(
+                value = textFieldState,
+                onValueChange = {
+                    textFieldState = it
+                },
+                label = {
+                    Text(text = "Enter your Name")
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                }
+            }) {
+                Text(text = "Please greet me")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun V2() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val color = remember {
+            mutableStateOf(Color.Yellow)
+        }
+        ColorBox(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1F)
+        ) {
+            color.value = this
+        }
+        Box(
+            modifier = Modifier
+                .background(color.value)
+                .weight(1F)
+                .fillMaxSize()
+        )
+    }
+}
+
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: Color.() -> Unit
+) {
+    Box(modifier = modifier
+        .background(color = Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1F
+                )
+            )
+        }
+    )
+}
+
+@Composable
+fun V1() {
+
+    val painter = painterResource(id = R.drawable.cat)
+    val description = "Cat"
+    val title = "This is my cat dude"
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.5F)
+                .padding(16.dp)
+        ) {
+
+            ImageCard(painter = painter, contentDescription = description, title = title)
+        }
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        Color.Red,
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append("J")
+                }
+                append("ETPACK")
+            },
+            color = Color.Red,
+            fontSize = 30.sp,
+        )
+
     }
 }
 
